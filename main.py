3f633ddff9bbcb58f8607161811b9627087ee7a5
canizalesJd <canizalesjd@gmail.com>
2024-08-20 19:27:22 -0600

    Adding FPS limit

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+            
+        screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
